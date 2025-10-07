06e0b9165a218e79c5b0bdab81cff11af591deaa
BhavanaBollampally <bhavanabollampally@gmail.com>
2025-10-08 01:38:34 +0530

    new formula added

--- fun.sh
+++ fun.sh
@@ -5,15 +5,20 @@ read -p "selling price : " sp
 echo "$sp"
 if [ "$cp" -gt "$sp" ]
 then
-	echo "loss is "$((cp - sp))""
+	echo "loss is $((cp - sp))"
 elif [ "$sp" -gt "$cp" ]
 then
-	echo "profit is "$((sp - cp))""
+	echo "profit is $((sp - cp))"
 else
 	echo " no profit no loss!"
 fi
 
+read -p "principal : " p
+echo "$p"
+read -p "time : " t
+echo "$t"
+read -p "rate : " r
+echo "$r"
 
-
-
-
+si=$(( (p * t * r) / 100))
+echo "simple intrest will be $si"
