9fcec72cfc50375ba2f7299f23338db9500aba26
BhavanaBollampally <bhavanabollampally@gmail.com>
2025-10-08 15:12:22 +0530

    Quick Command Logger

---
+++ sample.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+#Author:Bhavana_Mobile
+#Date: 08 oct 202
+#Task: Runs a command and dtores the output in file for multiple times
+read -p "Enter the command " com
+for i in {1..10}
+do
+	output=$($com)
+    echo "$output" > backup_${i}_$(date +%Y%m%d_%H%M%S).txt
+    sleep 1
+done
+
+echo "List of all the files created :"
+ls backup_*
+
+
+
